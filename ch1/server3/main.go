b210340efe8f667a7cf16a9fb2f106486d2bf532
Roger Wood Bixby <1766203+rbixby@users.noreply.github.com>
2019-05-13 22:01:47 -0700

    Fixed wrong print function.

--- ch1/server3/main.go
+++ ch1/server3/main.go
@@ -23,13 +23,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Header {
 		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 	}
-	fmt.Fprint(w, "Host= %q\n", r.Host)
-	fmt.Fprint(w, "RemoteAddr= %q\n", r.RemoteAddr)
+	fmt.Fprintf(w, "Host= %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
 	for k, v := range r.Form {
-		fmt.Fprint(w, "Form[%q] = %q\n", k, v)
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
 	}
 }
 
